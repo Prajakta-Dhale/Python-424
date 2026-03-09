1537909b9cb0b6990ca93a26cbc748fa4bfb390e
Prajakta <141712592+Prajakta-Dhale@users.noreply.github.com>
2026-03-09 09:27:48 +0000

    python

---
+++ Python 9th march/stringforloop.py
@@ -0,0 +1,53 @@
+#methods-count,find, replace,join,index,split,splitlines,
+#iterate string
+name="Itvedant"
+# for i in name:
+#     print(i)
+
+#reverse string
+# print("revere string:",name[::-1])
+
+# newstr=""
+# for i in name:
+#     newstr=i+newstr
+# print(newstr)
+
+#WAP newString without vowels
+# withoutvowel_str=""
+# for i in name:
+#     if i not in "aeiouAEIOU":
+#         withoutvowel_str=withoutvowel_str+i
+# print(withoutvowel_str)
+
+#WAP to print number of occurances of entered character from any string
+# str=input("Enter String=")
+# char=input("Enter Character=")
+# count=0
+# for i in str:
+#     if char==i:
+#         count+=1
+
+# print(f"{char} character count into {str} is {count}")
+
+#WAP to print number of occurances of entered character or word from any string
+str=input("Enter String=")
+chkstr=input("Enter char/word to find and show count=")
+count=0
+check=""
+for i in range(len(str)):
+    # print(i,str[i])
+    k=i
+    for j in range(len(chkstr)):
+        try:
+            check+=str[k]
+            k+=1
+        except:
+            None
+    if check==chkstr:
+        count+=1
+    check=""
+if count!=0:
+    print(f"{chkstr} found in {str} and count={count}")
+else:
+    print(f"{chkstr} not found in {str}")
+
