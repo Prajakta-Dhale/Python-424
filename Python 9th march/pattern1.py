e12085335b99b5da1c0dc1a0d7d4e102c87108be
Prajakta <141712592+Prajakta-Dhale@users.noreply.github.com>
2026-03-09 10:15:28 +0000

    left incremental triangle

--- Python 9th march/pattern1.py
+++ Python 9th march/pattern1.py
@@ -1,8 +1,69 @@
 # for i in range(3):
 #     print("*")
 
-for i in range(3):
-    for j in range(3):
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end="")
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end="")
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="")
+#     print()
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j,end="")
+#     print()
+
+# row=int(input("Enter Row count="))
+# column=int(input("Enter Column count="))
+# for i in range(row):
+#     for j in range(column):
+#         print("*",end="")
+#     print()
+
+
+# row=int(input("Enter Row count="))
+# column=int(input("Enter Column count="))
+# for i in range(row):
+#     for j in range(column):
+#         if i==j:
+#             print("1",end="")
+#         else:
+#             print("0",end="")
+    
+#     print()
+
+#left-incremental Triangle
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
         print("*",end="")
     print()
 
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print(i,end="")
+    print()
+
+count=int(input("Enter row count="))
+for i in range(count):
+    for j in range(i+1):
+        print(j,end="")
+    print()
+
+
+count=int(input("Enter row count="))
+n=1
+for i in range(count):
+    for j in range(i+1):
+        print(j,end="")
+        n+=1
+    print()
