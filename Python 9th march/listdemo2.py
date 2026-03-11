aaa0c6d54dcfa4392e090ff4c6d05430640d20b2
Prajakta <141712592+Prajakta-Dhale@users.noreply.github.com>
2026-03-11 08:11:21 +0000

    listdemo

---
+++ Python 9th march/listdemo2.py
@@ -0,0 +1,15 @@
+# # list1=[11,34,56,23,3,19]
+# # print(list1)
+
+# #operation
+# x=[1,2,3]
+# y=[4,5,6]
+# print(x+y)
+
+# # print(x+100) #TypeError: can only concatenate list (not "int") to list 
+# x.append(100)
+# #print(x) #[1, 2, 3, 100]
+
+# x[22]=222
+# print(X) #IndexError: list assignment index out of range
+
