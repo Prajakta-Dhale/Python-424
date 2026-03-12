80b373b36e658163a25f31604d37c3c4582bb8c6
Prajakta <141712592+Prajakta-Dhale@users.noreply.github.com>
2026-03-12 07:44:42 +0000

    listmethod

---
+++ Python 9th march/listdemo1.py
@@ -0,0 +1,42 @@
+books=[]
+while True:
+    print("--Library---")
+    print("1:AddBooks\n2:ShowBooks\n3:UpdateBooks\n4:DeleteBook\n5:Exit")
+    choice=int(input("Enter choice number:"))
+    if choice==1:
+        title=input("Enter books title:").capitalize()
+        if title not in books:
+            books.append(title)
+            print(f"{title} Book added succesfully!")
+        else:
+            print(f"{title} Book already exist so add another book")
+    elif choice==2:
+        if len(books)==0:
+            print("No books available try again")
+        else:
+            print("Available books are:",books)
+    elif choice==3:
+        if (len(books)>0):
+            old_book_title=input("Enter book title to update:").capitalize()
+            if old_book_title in books:
+                new_title=input("enter book new title to update:").capitalize()
+                books[books.index(old_book_title)]=new_title.capitalize()
+                print("Book title updated successfully!!")
+            else:
+                print(f"{old_book_title} Book title not found try again!!")
+        else:
+            print("No books available so first add the books!!")
+    elif choice==4:
+        if len(books)>0:
+            old_book_title=input("enter book title to remove:").capitalize()
+            if old_book_title in books:
+                books.remove(old_book_title.capitalize())
+                print(f"{old_book_title} Book removed successfully!!")
+            else:
+                print(f"{old_book_title} Book title not found try again!!")
+        else:
+            print("No book available so first add the books!!")
+            
+    elif choice==5:
+        print("Thank you for using our services!!")
+        break
---
+++ Python 9th march/listmethod.py
@@ -0,0 +1,17 @@
+fruits=["apple","grapes","banana","kiwi","watermelon"]
+print(fruits)
+fruits.reverse() #['watermelon', 'kiwi', 'banana', 'grapes', 'apple']
+print(fruits)
+fruits.sort()
+print(fruits)#['apple', 'banana', 'grapes', 'kiwi', 'watermelon']
+
+price=[200,300,100,500]
+fruits.extend(price)
+print(fruits)
+print(fruits.count("apple"))
+x=[1,2,3]
+print(x)
+x=fruits.copy()
+print(x)
+x.clear()
+print(x)
